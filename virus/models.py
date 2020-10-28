67228cd5d86371bf2fb9602826005c2b35bbfbd2
Bobby <tkdgus1797@gmail.com>
2020-10-28 11:36:48 +0900

    renamed a filed of virus model

--- virus/models.py
+++ virus/models.py
@@ -10,10 +10,10 @@ class Virus(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40)
     common_name = models.CharField(max_length=30)
-    maximum_period_of_infection = models.IntegerField()
+    max_infection_period = models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return self.namec
 
     class Meta:
         ordering = ['id']
